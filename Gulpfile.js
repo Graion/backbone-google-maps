af7e729402d6613b581f116254d662794f271671
aaccurso <accurso.alan@gmail.com>
2016-02-29 15:49:42 -0300

    Configures gulp build to concat src files

---
+++ Gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task('default', function() {
+  return gulp.src([
+      'src/models/**/*.js',
+      'src/collections/**/*.js',
+      'src/views/**/*.js',
+      'src/index.js'
+    ])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist'));
+});

f23a30d8a30672673c942fcdd968560d85ec75e0
aaccurso <accurso.alan@gmail.com>
2016-02-29 15:01:42 -0300

    Creates Location view and model

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-var Appointment = Backbone.Model.extend({});
+var App = (function($, Backbone, global) {
+  var init = function() {
+    var loc = new global.Location({
+      title: 'Graion',
+      latitude: 0.1,
+      longitude: 0.1
+    });
+
+    var locationView = new global.LocationView({model: loc});
+
+    locationView.render();
+
+    $('#app').html(locationView.el);
+  };
+
+  return {
+    init: init
+  };
+})(jQuery, Backbone, window);
+
+$(function() {
+  App.init();
+});
---
+++ src/views/Location.js
@@ -0,0 +1,5 @@
+var LocationView = Backbone.View.extend({
+  render: function() {
+    this.$el.html('<li>' + this.model.get('title') + '</li>');
+  }
+});

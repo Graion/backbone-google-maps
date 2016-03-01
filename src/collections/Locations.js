ad741ca64ea2a3aa978967729a3bbf6ecc67978a
Luciano Longo <luciano.longo@gmail.com>
2016-03-01 11:36:50 -0300

    Overwrite `add` to make collection unique

--- src/collections/Locations.js
+++ src/collections/Locations.js
@@ -1,3 +1,20 @@
 var Locations = Backbone.Collection.extend({
-  model: Location
+  model: Location,
+
+  /**
+   * Adds a location only once
+   * @param loc
+   * @returns {boolean}
+     */
+  add: function(loc) {
+    var dup = this.any(function(_loc) {
+      return (_loc.get('latitude') === loc.latitude && _loc.get('latitude') === loc.latitude) || _loc.get('title') === loc.title;
+    });
+
+    if (!dup) {
+      return Backbone.Collection.prototype.add.call(this, loc);
+    }
+
+    return false;
+  }
 });
